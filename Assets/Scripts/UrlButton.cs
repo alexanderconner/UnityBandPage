2e147c3edeee5f0358be50f101c6507e6ef40957
alexanderconner <astralbody888@gmail.com>
2020-04-05 22:02:25 -0400

    added new media links and icons.

---
+++ Assets/Scripts/UrlButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrlButton : MonoBehaviour
+{
+
+      public void OpenURL(string url)
+      {
+          Application.OpenURL(url);
+      }
+
+}
